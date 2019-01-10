3b22bcf33e067e18bbd321a643106096e3e5ca73
Nicolas Luzurier <hifrcy@gmail.com>
2019-01-10 10:43:10 +0100

    creation du projet

---
+++ src/main/java/com/girlcoder/start/atelierspring/AtelierSpringApplication.java
@@ -0,0 +1,14 @@
+package com.girlcoder.start.atelierspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelierSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelierSpringApplication.class, args);
+	}
+
+}
+
